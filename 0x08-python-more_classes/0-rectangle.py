a90a02bfe7c40cb92a26a912f45607c37bc07274
Monehin <monehinfeyisara.d@gmail.com>
2023-01-02 01:00:33 -0800

    project executed

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""A class that defines a rectangle"""
+
+
+class Rectangle:
+    """An empty class that represents a rectangle"""
+    pass
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privac
