988dced8e3a47dc1bb80b2846c6fcac4e009156c
Monehin <monehinfeyisara.d@gmail.com>
2023-01-12 03:43:17 -0800

    updated

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,38 +1,23 @@
 #!/usr/bin/python3
-"""module student"""
+""" creating student class """
+
 
 class Student:
-    """class student"""
-
+    """ Defining a class"""
     def __init__(self, first_name, last_name, age):
-        """Initialize instance student
-        """
+        """ Initializing a class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """return a dictionary representation of instance student
-        """
-        is_list = True
-        if type(attrs) is not list:
-            is_list = False
+        """ retrieves a dictionary representation """
+        if (type(attrs)) == list and all(type(i) == str for i in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         else:
-            for i in attrs:
-                if type(i) is not str:
-                    is_list = False
-
-        if not is_list:
             return self.__dict__
-        else:
-            dicti = {}
-            for attr in attrs:
-                if attr in self.__dict__:
-                    dicti[attr] = self.__dict__.get(attr)
-            return dicti
 
     def reload_from_json(self, json):
-        """replace all attributes of instance student
-        """
-        for idx, x in json.items():
-            self.__dict__[idx] = x
+        """update all the attributtes of student"""
+        for i in json:
+            self.__dict__.update({i: json[i]})
