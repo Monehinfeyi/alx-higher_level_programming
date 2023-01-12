97ae37259a4bc5f4e7a3b41d368a5a669233c5b7
Monehin <monehinfeyisara.d@gmail.com>
2023-01-12 03:36:59 -0800

    updated

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""module student"""
+""" creating student class """
+
 
 class Student:
-    """class student"""
-
+    """ Defining a class"""
     def __init__(self, first_name, last_name, age):
-        """Initialize instance of student
-        """
+        """ Initializing a class"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """retrieves a dictionary representation of a Student instance
-        """
+        """ converting to json """
         return self.__dict__
